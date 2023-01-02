6f08208be66e4b65f9751203c4da9455b9ae5afb
valentin baglan <vbaglan550@gmail.com>
2023-01-02 19:23:52 +0100

    Initial commit

---
+++ src/main/java/API/Generable.java
@@ -0,0 +1,14 @@
+package API;
+
+public interface Generable {
+
+    void Migration();
+    void Dispo_enseignant();
+    void Dispo_salle();
+    void Heures_cours();
+    void Ajout_heure_cours();
+    void ajout_heure_salle();
+
+
+
+}
---
+++ src/main/java/BDD/IntroductionJDBC/ProgrammePrinciapl.java
@@ -0,0 +1,27 @@
+package BDD.IntroductionJDBC;
+
+import Exploit.Exploitation;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+//On ne passera à l'étape n+1 que si l'étape n aboutie
+//si non on abandonne le programme
+
+
+public class ProgrammePrinciapl {
+
+
+
+    public static void main(String[] args) {
+
+
+        Exploitation exploitation = new Exploitation();
+        exploitation.Migration();
+
+    }
+
+}
